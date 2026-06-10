ebe51b4755b72da7128dccc3570e96fe76379574
Sara <s.cristal.p@gmail.com>
2026-06-10 19:13:31 +0200

    index.js and some boiler plate

---
+++ index.js
@@ -0,0 +1,29 @@
+const username = prompt("What's your name?");
+alert(`Hello, ${username}!`);
+
+let score = 0;
+function addFive(userInput) {
+    const result = 1 + userInput
+    return result
+}
+
+const guess = 10
+const newScore = addFive(guess)
+
+const question1 = prompt(`Please select one of the following options.
+[1] Pretzels ($1.99)
+[2] Chips ($0.99)
+[3] Popcorn ($1.49)
+`);
+
+const question2 = prompt(`Please select one of the following options.
+[1] Pretzels ($1.99)
+[2] Chips ($0.99)
+[3] Popcorn ($1.49)
+`);
+
+const question3 = prompt(`Please select one of the following options.
+[1] Pretzels ($1.99)
+[2] Chips ($0.99)
+[3] Popcorn ($1.49)
+`);
